84e61a3e7dd85f00a11fe2eaad58a37661d42013
lgd <lg.durand@protonmail.com>
2019-09-18 12:13:41 +0200

    Add snippets & doc, remove Java SE headers

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import * as vscode from 'vscode';
 import { getStandardOsgiHeaders } from './completion/osgi';
 import { getBndOsgiHeaders, getBndInstructionHeaders } from './completion/bnd';
 import { getLiferayOsgiHeaders } from './completion/liferay';
-import { getJavaHeaders } from './completion/java';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -12,14 +11,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
-			const javaHeaders = getJavaHeaders();
 			const standardOsgiHeaders = getStandardOsgiHeaders();
 			const bndOsgiHeaders = getBndOsgiHeaders();
 			const bndInstructionsHeaders = getBndInstructionHeaders();
 			const liferayOsgiHeaders = getLiferayOsgiHeaders();
 
-			const headers = javaHeaders
-				.concat(standardOsgiHeaders)
+			const headers = standardOsgiHeaders
 				.concat(bndOsgiHeaders)
 				.concat(bndInstructionsHeaders)
 				.concat(liferayOsgiHeaders);
