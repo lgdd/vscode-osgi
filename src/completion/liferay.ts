3b501e07c83e08b8548b13dd31e8b89168ddb0d2
André Biegel <andre.biegel@icloud.com>
2019-10-08 22:06:44 +0200

    Update liferay.ts

--- src/completion/liferay.ts
+++ src/completion/liferay.ts
@@ -5,7 +5,7 @@ import { generateCompletionItems } from './common';
 // https://github.com/liferay/liferay-portal/blob/master/modules/util/source-formatter/documentation/bnd_schema_version.markdown
 import headers from './data/liferay-headers.json'
 
-import headers from './data/liferay-instructions.json'
+import instructions from './data/liferay-instructions.json'
 
 export function getLiferayOsgiHeaders() {
     return generateCompletionItems(headers);
